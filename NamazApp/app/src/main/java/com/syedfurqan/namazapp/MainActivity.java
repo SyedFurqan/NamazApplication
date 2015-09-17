37a7bd1640c1abefa59ebf2e1a7218ccdf36bd5b
Syed furqan <syedfurqan96@gmail.com>
2015-09-17 17:17:08 +0500

    gfgf

--- NamazApp/app/src/main/java/com/syedfurqan/namazapp/MainActivity.java
+++ NamazApp/app/src/main/java/com/syedfurqan/namazapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
     String abc="hello";
+    int fr=12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
