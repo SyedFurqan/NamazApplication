555bc72e98d901b5850b230b150e00c8f9500140
Ay-Bee <abdulbst1994@gmail.com>
2015-09-19 21:41:27 +0500

    9/19/2015 Basit's Home

---
+++ NamazApp/app/src/main/java/com/syedfurqan/namazapp/ArabicWordsArraylists.java
@@ -0,0 +1,21 @@
+package com.syedfurqan.namazapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Abdul Basit on 9/19/2015.
+ */
+public class ArabicWordsArraylists {
+    ArrayList arrayList1;
+
+    public ArrayList getArrayList1(){
+        arrayList1=new ArrayList();
+        arrayList1.add("الله اكبر");
+        arrayList1.add("الله و اكبر");
+        arrayList1.add("الله واكبر");
+        arrayList1.add("و الله اكبر");
+        arrayList1.add("الله و اكبر و");
+
+        return arrayList1;
+    }
+}
--- NamazApp/app/src/main/java/com/syedfurqan/namazapp/MainActivity.java
+++ NamazApp/app/src/main/java/com/syedfurqan/namazapp/MainActivity.java
@@ -1,16 +1,105 @@
 package com.syedfurqan.namazapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Vibrator;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
-    String abc="hello";
-    int fr=12;
-    String abdulbasit;
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    private SpeechRecognizer sr;
+    Vibrator v;
+    String result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void startListening(View view) {
+        if(sr!=null){
+            Log.d("22222222", "Destroy");
+            sr.destroy();
+        }
+        sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener(new listener());
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "ar-eg");
+        intent.putExtra("android.speech.extra.EXTRA_ADDITIONAL_LANGUAGES", new String[]{});
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "ar-eg");
+        intent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, "ar-eg");
+        sr.startListening(intent);
+    }
+
+    class listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params)
+        {
+
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d("Message........","Listening");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d("Message........", "Stopped");
+        }
+        public void onError(int error)
+        {
+            Log.d("Message........", "Error" + error);
+        }
+        public void onResults(Bundle results)
+        {
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d("Current Result........",data.get(i).toString());
+            }
+            if(data.size()>0){
+                result=data.get(0).toString();
+
+                check(result);
+            }
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+
+        }
+    }
+
+    private void check(String result) {
+        ArrayList array_list1=(new ArabicWordsArraylists()).getArrayList1();
+        for (int i=0;i<array_list1.size();i++){
+            if (array_list1.contains(result)){
+                Log.d("Message......","Result Matched");
+                break;
+            }
+        }
     }
 }
